1b8fff07232ffeff4f7cfb11ac2caad2dd95fdb5
rafaellevy <srlevy2@gmail.com>
2021-08-30 17:05:42 -0700

    First react component created

---
+++ src/components/BookView.js
@@ -0,0 +1,11 @@
+function BookView() {
+  return (
+    <div>
+      <img src="https://pictures.abebooks.com/inventory/30329881180.jpg" />
+      <h1>Git for Dummies</h1>
+      <p>GitHub For Dummies is the next step on your journey as a developer.</p>
+    </div>
+  );
+}
+
+export default BookView;
